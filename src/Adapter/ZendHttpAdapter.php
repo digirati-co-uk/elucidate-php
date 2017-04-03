8bf0fa9cfa4017aee8d798228ba405b081f9e214
Stephen Fraser <stephen.fraser@digirati.co.uk>
2017-04-03 14:55:21 +0100

    Updated travis

--- src/Adapter/ZendHttpAdapter.php
+++ src/Adapter/ZendHttpAdapter.php
@@ -1,6 +1,7 @@
 <?php
 namespace Elucidate\Adapter;
 
+use Elucidate\Model\RequestModel;
 use Zend\Http\Client;
 
 class ZendHttpAdapter implements HttpAdapter
@@ -10,5 +11,25 @@ class ZendHttpAdapter implements HttpAdapter
     public function __construct(Client $client)
     {
         $this->client = $client;
+    }
+
+    public function post(string $endpoint, RequestModel $request): string
+    {
+        // TODO: Implement post() method.
+    }
+
+    public function put(string $endpoint, RequestModel $request): string
+    {
+        // TODO: Implement put() method.
+    }
+
+    public function delete(RequestModel $request): bool
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public function get(string $endpoint, array $headers = []): string
+    {
+        // TODO: Implement get() method.
     }
 }

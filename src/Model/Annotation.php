4fef32e55392e04c5d1501253ca198103bda12e8
scottmelville <scott.melville@digirati.com>
2017-04-20 15:23:19 +0100

    Adding fields to annotation model for creator and generator

--- src/Model/Annotation.php
+++ src/Model/Annotation.php
@@ -9,36 +9,42 @@ use LogicException;
 
 class Annotation implements RequestModel, ResponseModel, ArrayAccessInterface
 {
-    use JsonLDContext;
-    use SerializeToJsonLD;
-    use ArrayAccess;
+  use JsonLDContext;
+  use SerializeToJsonLD;
+  use ArrayAccess;
 
-    private $type;
-    private $body;
-    private $target;
-    private $id;
-    private $container;
+  private $type;
+  private $body;
+  private $target;
+  private $id;
+  private $creator;
+  private $generator;
+  private $container;
 
-    public function getContainer()
-    {
-        return $this->container;
-    }
+  public function getContainer()
+  {
+    return $this->container;
+  }
 
-    public function __construct(
-        string $id = null,
-        $body = null,
-        $target = null,
-        Container $container = null
-    ) {
-        $this->type = 'Annotation';
-        $this->body = $body;
-        $this->target = $target;
-        $this->id = $id;
-        $this->container = $container;
-    }
+  public function __construct(
+    string $id = null,
+    $body = null,
+    $target = null,
+    $creator = null,
+    $generator = null,
+    Container $container = null
+  ) {
+    $this->type = 'Annotation';
+    $this->body = $body;
+    $this->target = $target;
+    $this->id = $id;
+    $this->creator = $creator;
+    $this->generator = $generator;
+    $this->container = $container;
+  }
 
-    public function __toString()
-    {
-        return $this->id;
-    }
+  public function __toString()
+  {
+    return $this->id;
+  }
 }

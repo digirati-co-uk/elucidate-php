f6c48236703ad1c3b7bf3b1aaf0f23a571f2efaa
Stephen Fraser <stephen.fraser@digirati.co.uk>
2017-06-15 14:38:43 +0100

    IDA-330 Added more to create fluent APi

--- src/Model/SearchResult.php
+++ src/Model/SearchResult.php
@@ -2,6 +2,8 @@
 
 namespace Elucidate\Model;
 
+use Elucidate\Search\SearchCustom;
+
 class SearchResult
 {
     private $container;
@@ -9,6 +11,12 @@ class SearchResult
     public function __construct(Container $container)
     {
         $this->container = $container;
+    }
+
+    /** @return SearchCustom|null */
+    public function getNextSearchQuery()
+    {
+        return new SearchCustom($this->container['first']['next']) ?? null;
     }
 
     public function getNextPage()
@@ -24,5 +32,15 @@ class SearchResult
         foreach ($this->container['first']['items'] as $item) {
             yield Annotation::fromArray($item);
         }
+    }
+
+    public static function fromArray(array $data) : SearchResult
+    {
+        return new static(Container::fromArray($data));
+    }
+
+    public static function fromJson(string $json) : SearchResult
+    {
+        return new static(Container::fromJson($json));
     }
 }

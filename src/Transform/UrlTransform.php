51974728a250c989ec02b76ea1571a21b6b1ba31
stephenwf <stephen.fraser@digirati.com>
2017-09-19 18:15:09 +0100

    Added URL transform

---
+++ src/Transform/UrlTransform.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Elucidate\Transform;
+
+use Elucidate\Model\Annotation;
+use Elucidate\Model\Container;
+use InvalidArgumentException;
+use Zend\Uri\Uri;
+
+
+class UrlTransform
+{
+    private $uri;
+
+    public function __construct(string $uri)
+    {
+        $this->uri = new Uri($uri);
+    }
+
+    private function transformUri($url) {
+        $newUrl = new Uri($url);
+        $newUrl->setHost($this->uri->getHost());
+        $newUrl->setPort($this->uri->getPort());
+        $newUrl->setScheme($this->uri->getScheme());
+        return $newUrl->toString();
+    }
+
+    public function transformAnnotation(Annotation $annotation) : Annotation
+    {
+        $fields = $annotation->jsonSerialize();
+        $fields['id'] = $this->transformUri($annotation['id']);
+        return Annotation::fromArray($fields);
+    }
+
+    public function transformContainer(Container $container) : Container
+    {
+        $fields = $container->jsonSerialize();
+        // JSON.id
+        $fields['id'] = $this->transformUri($container['id']);
+
+        // JSON.first.items
+        if (isset($fields['first']['items'])) {
+            $fields['first']['items'] = array_map(function($annotation) {
+                $annotation['id'] = $this->transformUri($annotation['id']);
+                return $annotation;
+            }, $fields['first']['items']);
+        }
+
+        // JSON.first.partOf
+        if (isset($fields['first']['partOf'])) {
+            $fields['first']['partOf'] = $this->transformUri($container['first']['partOf']);
+        }
+
+        // JSON.last
+        if (isset($fields['last'])) {
+            $fields['last'] = $this->transformUri($container['last']);
+        }
+
+        return Container::fromArray($fields);
+    }
+
+    public function __invoke($annotationOrContainer)
+    {
+        if ($annotationOrContainer instanceof Annotation) {
+            return $this->transformAnnotation($annotationOrContainer);
+        }
+        if ($annotationOrContainer instanceof Container) {
+            return $this->transformContainer($annotationOrContainer);
+        }
+        throw new InvalidArgumentException("Unsupported type in Url Transform: " . get_class($annotationOrContainer));
+    }
+}
---
+++ tests/src/Transform/UrlTransformTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Elucidate\Tests\Transform;
+
+use Elucidate\Model\Annotation;
+use Elucidate\Model\Container;
+use Elucidate\Transform\UrlTransform;
+use PHPUnit\Framework\TestCase;
+
+class UrlTransformTest extends TestCase
+{
+
+    public function test_url_transform_basic_annotation()
+    {
+        $annotation = new Annotation('http://google.com/123/456');
+        $transform = new UrlTransform('https://yahoo.com');
+
+        $newAnnotation = $transform($annotation);
+        $this->assertEquals($newAnnotation['id'], 'https://yahoo.com/123/456');
+    }
+
+    public function test_url_transform_full_container()
+    {
+        $containerJson = file_get_contents(__DIR__ . '/container.json');
+        $container = Container::fromJson($containerJson);
+
+        $transform = new UrlTransform('https://yahoo.com');
+        $newContainer = $transform($container);
+
+        $this->assertEquals($newContainer['id'], 'https://yahoo.com/annotation/w3c/0fe60b581d19c5c8203e3ec8870d196a/');
+
+        $this->assertFalse(strpos(json_encode($newContainer), 'elucidate'));
+    }
+
+}
